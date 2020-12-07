44219187b5db80424cbd4ed02d7750d835591548
Cody-dev-debug <bansaladitya150@gmail.com>
2020-12-07 20:31:36 +0530

    First Commit

---
+++ Task-4/C.c
@@ -0,0 +1,73 @@
+#include <stdio.h>
+#include<stdbool.h>
+int a;
+
+void printBoard(int board[a][a])
+{
+    for(int i=0; i<a;i++)
+	    {for(int j=0;j<a;j++)
+	    printf("%d",board[i][j]);
+	    printf("\n");}
+}
+
+bool attacked(int board[a][a], int x, int y)
+{
+	for(int i=0;i<a;i++)
+	{
+		if(board[x][i]==1)
+		return true;
+		if(board[i][y]==1)
+		return true;
+	}
+	for(int i=0;i<a;i++)
+	{
+		for(int j=0;j<a;j++)
+		{
+			if(board[i][j] == 1 && i+j==x+y)
+			return true;
+			if(board[i][j] == 1 && i-j==x-y)
+			return true;
+		}
+	}
+	return false;
+}
+
+bool queen(int board[a][a],int n)
+{
+    // printf("Hi%d\n", n);
+    // printBoard(board);
+	if(n==0)
+	return true;
+	int col = a - n;
+	for(int i=0;i<a;i++)
+	{
+		for(int j=0;j<a;j++)
+		{
+			if(attacked(board,i,j)) {
+			 //   printf("%d\n", n);
+			    continue;
+			}
+			board[i][j]=1;
+            // printBoard(board);
+			if(queen(board,n-1))
+			return true;
+			board[i][j] = 0;
+ 		}
+	}
+	return false;
+}
+void main()
+{
+	printf("Enter the size of board\n");
+	scanf("%d",&a);
+	int board[a][a];
+	for(int i=0;i<a;i++)
+	for(int j=0;j<a;j++)
+	board[i][j]=0;
+	if(queen(board,a))
+	{
+		printf("YES\n");
+		printBoard(board);
+	}
+	else printf("NO");
+}
